d2915e22da3de37e9da3997f873225be526c301e
Renee Mumbi <1723renee@gmail.com>
2022-12-08 15:59:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(records) {
+    let year;
+    records.find(function (item) {
+        if (item.result === "W") {
+            //console.log(item.year)
+            return year = item.year
+        }
+        
+    })
+    return year
+    
+}
+superbowlWin
